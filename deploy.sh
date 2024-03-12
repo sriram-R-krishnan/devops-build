cfa76b4451e51f49f09559bc9ea993fe70b49bff
Arun dinesh <126953533+ArunDineshchandran@users.noreply.github.com>
2024-03-12 13:16:40 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,10 @@
 docker tag project:v1 arundinesh3005/prod
 docker tag project:v1 arundinesh3005/dev
 
+
+# Docker login
+docker login -u arundinesh3005 -p dckr_pat_96ijsIXskEZUTCq5T8c0VNq9zhw
+
 # Push the image to Docker Hub
 docker push arundinesh3005/prod
 docker push arundinesh3005/dev
