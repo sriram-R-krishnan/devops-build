b1027ce91c9bb32caaa5ff38ba8282070bc31855
SumiSatish <157936646+SumiSatish@users.noreply.github.com>
2024-03-23 16:33:30 +0530

    Delete build/deploy.sh

--- build/deploy.sh
+++
@@ -1,8 +0,0 @@
-# Stop all running containers
-sudo docker stop $(docker ps -a -q)
-
-# Remove all containers
-sudo docker rm $(docker ps -a -q)
-
-# Start new container with the latest image
-sudo docker-compose up -d
