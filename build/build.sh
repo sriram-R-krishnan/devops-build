eef03eae463a73b4199bc2afbcaae4638e293f5c
SumiSatish <157936646+SumiSatish@users.noreply.github.com>
2024-03-23 16:33:16 +0530

    Delete build/build.sh

--- build/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo docker stop $(docker ps -a -q)
-sudo docker rm $(docker ps -a -q)
-sudo docker image rmi -f sumithrasatish/prod:react-app
-sudo docker image rmi -f sumithrasatish/dev:react-app
-sudo docker rmi -f react-app:latest
-sudo  docker rmi -f node:latest
-sudo docker build -t react-app:latest .
